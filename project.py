a4ef7e033dd1fb93b23a10a31b5f9077fcda05ed
Joshua Marion <78986694+JoshuaMarion@users.noreply.github.com>
2023-02-27 17:24:20 -0500

    Initial commit with code
    Does not do much now lmao

---
+++ project.py
@@ -0,0 +1,12 @@
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    queue = []
+    n = input()
+    processes = input()
+    print(f'<<< PROJECT PART I -- process set (n={n}) with {processes} CPU-bound process >>>')
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
